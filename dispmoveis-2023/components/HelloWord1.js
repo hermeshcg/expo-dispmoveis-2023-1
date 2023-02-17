11d14b57a35c4de5c4389edd4c1dbd5bfc9e65fe
hermeshcg <hermescaretta@gmail.com>
2023-02-16 22:07:18 -0300

    :fire: new codes

--- dispmoveis-2023/components/HelloWord1.js
+++ dispmoveis-2023/components/HelloWord1.js
@@ -1,21 +1,25 @@
-import {View, Text} from 'react-native'
+import { View, Text } from 'react-native';
 
-export default function HelloWord1 ({text}){
-  return(
-    <View style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "green"
-    }}>
-      <Text  style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "green"
-    }}>
+export default function HelloWord1({ text }) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green',
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          fontWeight: 'bold',
+          color: 'red',
+          textTransform: 'uppercase',
+        }}
+      >
         {text}
       </Text>
     </View>
-  )
+  );
 }
